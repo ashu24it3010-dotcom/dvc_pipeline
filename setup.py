291948bd83dd8e31e5af16c7426a813e1edd8ca3
Ashutosh kumar <ashu24it3010@gmail.com>
2026-06-25 13:33:27 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='understanding dvc_pipeline and data versioning',
+    author='campusx',
+    license='',
+)
